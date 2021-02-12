b7f65709bc0390e2b5e834ed14450ed29767f4c6
Dibas Dauliya <dibasdauliya@gmail.com>
2021-02-12 23:09:21 +0545

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## App Open Ads and Interistial Ads Implementaion
+
+Helpful Link: [App Open Ads Docs](https://developers.google.com/admob/android/app-open-ads)
---
+++ app/src/main/java/com/example/ads/MyApplication.java
@@ -0,0 +1,27 @@
+package com.example.ads;
+
+import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+/**
+ * The Application class that manages AppOpenManager.
+ */
+public class MyApplication extends Application {
+    private static AppOpenManager appOpenManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileAds.initialize(
+                this,
+                new OnInitializationCompleteListener() {
+                    @Override
+                    public void onInitializationComplete(InitializationStatus initializationStatus) {
+                    }
+                });
+        appOpenManager = new AppOpenManager(this);
+    }
+}
